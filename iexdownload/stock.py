10199d17c032f51d4acb2887ee184bfc37a5a0ff
Shawn Lin <xiaoliang.lin@columbia.edu>
2019-12-06 02:33:09 -0500

    Renamed file

--- iexdownload/stock.py
+++ iexdownload/stock.py
@@ -1,11 +1,8 @@
 import requests
 
-from iexdownload.constants import *
 
-
-def get_stock_prices_historical(symbol: str, range: str):
+def get_historical_price(symbol: str, range: str):
 
     api_query = f"{IEX_CLOUD}/stock/{symbol}/chart/{range}?token={IEX_TOKEN}"
     print(api_query)
     return requests.get(api_query).json()
-
