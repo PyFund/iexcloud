477fb3767ad5256a2c0ba5e248339466bb2f838d
Shawn Lin <xiaoliang.lin@columbia.edu>
2019-12-13 02:15:49 -0500

    Add support for pandas, and store output in instance

--- iexcloud/stock.py
+++ iexcloud/stock.py
@@ -1,12 +1,51 @@
+import time
+
+import pandas as pd
 import requests
+from requests import Response
 
 from iexcloud.constants import IEX_CLOUD, IEX_TOKEN
 
 
 class Stock(object):
 
-    def __init__(self, symbol: str):
-        self.symbol = symbol
+    def __init__(self, symbol: str, output="json"):
+
+        self.symbol: str = symbol
+        self.output: str = output
+        self.dividend = None
+        self.earning = None
+        self.logo = None
+        self.news = None
+        self.peer = None
+        self.price = None
+        self.profile = None
+        self.sentiment = None
+        self.split = None
+
+    def _response_text_to_pd(self, response_text: str):
+
+        response_text = response_text.replace("'", '"')
+
+        if not response_text.startswith("[") and not response_text.endswith("]"):
+            response_text = "[" + response_text + "]"
+
+        df = pd.read_json(response_text)
+        df['symbol'] = self.symbol
+        df['retrieved'] = time.time()
+
+        return df
+
+    def _create_output(self, response: Response):
+
+        if response:
+            cases = {
+                "json": response.text,
+                "pandas": self._response_text_to_pd(response.text)
+            }
+            return cases[self.output]
+        else:
+            raise response.raise_for_status()
 
     def get_dividend(self, time_range: str):
         """https://iexcloud.io/docs/api/#dividends-basic
@@ -22,9 +61,11 @@ class Stock(object):
 
         api_url = f"{IEX_CLOUD}/stock/{self.symbol}/dividends/{time_range}?"
         response = requests.get(api_url, params={'token': IEX_TOKEN})
+        output = self._create_output(response)
 
-        if response:
-            return response.json()
+        self.dividend = output
+
+        return output
 
     def get_earning(self, last: int):
         """https://iexcloud.io/docs/api/#earnings
@@ -40,9 +81,11 @@ class Stock(object):
 
         api_url = f"{IEX_CLOUD}/stock/{self.symbol}/earnings/{last}?"
         response = requests.get(api_url, params={'token': IEX_TOKEN})
+        output =  self._create_output(response)
 
-        if response:
-            return response.json()
+        self.earning = output
+
+        return output
 
     def get_logo(self):
         """https://iexcloud.io/docs/api/#logo
@@ -54,9 +97,11 @@ class Stock(object):
 
         api_url = f"{IEX_CLOUD}/stock/{self.symbol}/logo?"
         response = requests.get(api_url, params={'token': IEX_TOKEN})
+        output =  self._create_output(response)
 
-        if response:
-            return response.json()
+        self.logo = output
+
+        return output
 
     def get_news(self, last: int):
         """https://iexcloud.io/docs/api/#news
@@ -72,9 +117,12 @@ class Stock(object):
 
         api_url = f"{IEX_CLOUD}/stock/{self.symbol}/news/last/{last}"
         response = requests.get(api_url, params={'token': IEX_TOKEN})
+        output = self._create_output(response)
 
-        if response:
-            return response.json()
+        self.news = output
+
+        return output
+
 
     def get_peer(self):
         """https://iexcloud.io/docs/api/#peer-groups
@@ -86,9 +134,11 @@ class Stock(object):
 
         api_url = f"{IEX_CLOUD}/stock/{self.symbol}/peers"
         response = requests.get(api_url, params={'token': IEX_TOKEN})
+        output = self._create_output(response)
 
-        if response:
-            return response.json()
+        self.peer = output
+
+        return output
 
     def get_price(self, time_range: str):
         """
@@ -105,9 +155,11 @@ class Stock(object):
 
         api_url = f"{IEX_CLOUD}/stock/{self.symbol}/chart/{time_range}"
         response = requests.get(api_url, params={'token': IEX_TOKEN})
+        output = self._create_output(response)
 
-        if response:
-            return response.json()
+        self.price = output
+
+        return output
 
     def get_profile(self):
         """https://iexcloud.io/docs/api/#company
@@ -119,9 +171,11 @@ class Stock(object):
 
         api_url = f"{IEX_CLOUD}/stock/{self.symbol}/company"
         response = requests.get(api_url, params={'token': IEX_TOKEN})
+        output = self._create_output(response)
 
-        if response:
-            return response.json()
+        self.profile = output
+
+        return output
 
     def get_sentiment(self, date):
         """https://iexcloud.io/docs/api/#social-sentiment
@@ -138,8 +192,11 @@ class Stock(object):
         api_url = f"{IEX_CLOUD}/stock/{self.symbol}/sentiment/mute/{date}"
         response = requests.get(api_url, params={'token': IEX_TOKEN})
 
-        if response:
-            return response.json()
+        output = self._create_output(response)
+
+        self.sentiment = output
+
+        return output
 
     def get_split(self, time_range: str):
         """https://iexcloud.io/docs/api/#splits-basic
@@ -156,5 +213,13 @@ class Stock(object):
         api_url = f"{IEX_CLOUD}/stock/{self.symbol}/splits/{time_range}"
         response = requests.get(api_url, params={'token': IEX_TOKEN})
 
-        if response:
-            return response.json()
+        output = self._create_output(response)
+
+        self.split = output
+
+        return output
+
+
+test = Stock("AAPL", output="pandas")
+print(test.get_logo())
+print(test.logo)
