fdc98ca309b405bf666617bd2d03cc748af354da
Shawn Lin <xiaoliang.lin@columbia.edu>
2020-07-15 23:01:25 +0000

    formatting

--- setup.py
+++ setup.py
@@ -2,26 +2,26 @@ from setuptools import setup
 
 import versioneer
 
-DISTNAME = 'iexcloud'
+DISTNAME = "iexcloud"
 DESCRIPTION = "iexcloud retrieves and archives iexcloud data in databases"
-MAINTAINER = 'Shawn Lin'
-MAINTAINER_EMAIL = 'shawnlin.xl@gmail.com'
-AUTHOR = 'Shawn Lin'
-AUTHOR_EMAIL = 'shawnlin.xl@gmail.com'
+MAINTAINER = "Shawn Lin"
+MAINTAINER_EMAIL = "shawnlin.xl@gmail.com"
+AUTHOR = "Shawn Lin"
+AUTHOR_EMAIL = "shawnlin.xl@gmail.com"
 URL = "https://github.com/shawnlinxl/iexcloud"
-LICENSE = "GPLv3"
+LICENSE = "MIT"
 
-classifiers = ['Development Status :: 1 - Planning',
-               'Programming Language :: Python :: 3.7',
-               'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-               'Intended Audience :: Science/Research',
-               'Topic :: Office/Business :: Financial',
-               'Topic :: Scientific/Engineering',
-               'Operating System :: OS Independent']
-
-install_reqs = [
-    "requests>=2.22.0"
+classifiers = [
+    "Development Status :: 1 - Planning",
+    "Programming Language :: Python :: 3.7",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Intended Audience :: Science/Research",
+    "Topic :: Office/Business :: Financial",
+    "Topic :: Scientific/Engineering",
+    "Operating System :: OS Independent",
 ]
+
+install_reqs = ["requests>=2.22.0"]
 
 if __name__ == "__main__":
     setup(
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         classifiers=classifiers,
         install_requires=install_reqs,
         url=URL,
-        packages=['iexcloud'],
+        packages=["iexcloud"],
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
     )
